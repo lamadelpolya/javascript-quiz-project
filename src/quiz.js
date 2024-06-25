e0553db9b506fc615d34f20753447586e87d6f3b
Polina Yukhymenko <polya.kiev@gmail.com>
2024-06-25 14:41:56 +0200

    day2

--- src/quiz.js
+++ src/quiz.js
@@ -12,28 +12,46 @@ class Quiz {
   moveToNextQuestion() {
     this.currentQuestionIndex++;
   }
-  shuffleQuestions () {
+  shuffleQuestions() {
     for (let i = this.questions.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
-      }
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
   }
-//   checkAnswer (answer){
-//     if (answer === this.questions[this.currentQuestionIndex].answer) {
-//       this.correctAnswers++;
-//     }
-//     this.moveToNextQuestion();
-    checkAnswer(answer) {
-        const currentQuestion = this.getQuestion();
-        if (currentQuestion.answer === answer) {
-          this.correctAnswers++;
-        }
-      }
-}
-   
-hasEnded() {
+  //   checkAnswer (answer){
+  //     if (answer === this.questions[this.currentQuestionIndex].answer) {
+  //       this.correctAnswers++;
+  //     }
+  //     this.moveToNextQuestion();
+  checkAnswer(answer) {
+    const currentQuestion = this.getQuestion();
+    if (currentQuestion.answer === answer) {
+      this.correctAnswers++;
+    }
+  }
+  hasEnded() {
     return (
       this.currentQuestionIndex >= this.questions.length ||
       this.timeRemaining <= 0
     );
   }
+
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter(
+        (question) => question.difficulty === difficulty
+      );
+    }
+  }
+  averageDifficulty() {
+    if (this.questions.length === 0) return 0;
+    const totalDifficulty = this.questions.reduce(
+      (sum, question) => sum + question.difficulty,
+      0
+    );
+    return totalDifficulty / this.questions.length;
+  }
+}
